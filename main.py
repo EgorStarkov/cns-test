873c6a6ec6c3dc985ef98747bd1866a659128fae
estarkov <estarkov.sagirov@gmail.com>
2024-06-10 17:06:04 +0500

    bcvx

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def run_selenium_test():
 
 
             # Ожидание, чтобы товар добавился в корзину
-            time.sleep(random.uniform(6, 8))
+            time.sleep(random.uniform(15, 20))
 
             # Переход в корзину
             try:
